dd0b6e3b2be2a38b0d72cd3bebeccf2886d455ca
Chillzfelix <Chillzfelix01@gmail.com>
2023-03-24 18:11:11 -0700

    u

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,18 @@
 #include "main.h"
-
 /**
-* print_most_numbers - prints from 0 to 9 without 2 and 4
-*
-* Return: Void
+* print_most_numbers - prints the numbers from 0 to 9, followed by a new line,
+*                      but skips 2 and 4
 */
 void print_most_numbers(void)
 {
-int c;
-for (c = 0; c <= 9; c++)
+int i;
+for (i = 0; i < 10; i++)
 {
-if (c != 2 || c != 4)
+if (i == 2 || i == 4)
 {
-_putchar(c + '0');
+continue;
 }
+_putchar(i + '0');
 }
 _putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_square - prints a square of a given size
+* @size: size of the square to prin
+* Return: void
+*/
+void print_square(int size)
+{
+int row, column;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (row = 1; row <= size; row++)
+{
+for (column = 1; column <= size; column++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
