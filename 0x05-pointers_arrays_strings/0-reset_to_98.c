6b8508a38706fda10b82739c38e9297e9c39690c
Chillzfelix <Chillzfelix01@gmail.com>
2023-03-27 15:01:06 -0700

    z

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - sets the address of int num to pointer n
+ *
+ * @n: Pointer to set to 98
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
