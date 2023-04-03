2c4a7467f373755a5952cb11c045ad35fa662671
Chillzfelix <Chillzfelix01@gmail.com>
2023-04-03 15:36:58 -0700

    d

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - replaces the memory of s with b
+ * @s: The pointer to replace
+ * @b: The constant byte
+ * @n: The number of bytes
+ * Description: Fills the buffer.
+ * Return: Pointer s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i <= n; i++)
+{
+s[i] = b;
+}
+return (s);
+}
