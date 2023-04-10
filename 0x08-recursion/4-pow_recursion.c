329678451da383c4ac85ff41bc3199b37d5dddd0
Chillzfelix <Chillzfelix01@gmail.com>
2023-04-10 14:01:25 -0700

    w

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,7 +15,5 @@ if (y-- 0)
 {
 return (1);
 }
-{
 return (x * _pow_recursion(x, y - 1));
-}
 }
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,7 +3,7 @@
 * _sqrt_recursion - returns the natural square root of a number
 * @n: number to calculate the square root of
 *
-* return: the resulting square root
+* Return: the resulting square root
 */
 int _sqrt_recursion(int n)
 {
@@ -19,7 +19,7 @@ int _sqrt_recursion(int n)
 * @n: number to calculate the square root
 * @i: iterator
 *
-* return: the resulting square root
+* Return: the resulting square root
 */
 int actual_sqrt_recursion(int n, int i)
 {
