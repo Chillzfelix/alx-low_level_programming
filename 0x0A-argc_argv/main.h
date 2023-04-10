6451182e90b019e1339ecc314e8178e3ea0f9966
Chillzfelix <Chillzfelix01@gmail.com>
2023-04-10 10:00:41 -0700

    k

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,10 @@
 #define main_h
 
 int _putchar(char c);
+void func_name(int argc, char *argv[]);
+void num_arg(int argc, char *argv[]);
+void print_arg(int argc, char *argv[]);
+int multiply(int a, int b);
+int add(int a, int b);
 
 #endif
