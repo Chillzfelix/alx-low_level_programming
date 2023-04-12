985e3e52970b3377451704c304852fb7a8a33152
Chillzfelix <Chillzfelix01@gmail.com>
2023-04-12 00:43:07 -0700

    f

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * function_name - prints the name function from the command line
+ * func_name - prints the name function from the command line
  * @argc: Number of arguement
  * @argv: Array of argument
  *
  * Return: Always 0 (success)
  */
-int main(int argc__attribute__((unused)), char *argv[])
+int  main(int argc __attribute__((unused)), char *argv[])
 {
 print("%s\n", *argv);
 return (0);
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,10 +2,6 @@
 #define main_h
 
 int _putchar(char c);
-void function_name(int argc, char *argv[]);
-void num_arg(int argc, char *argv[]);
-void print_arg(int argc, char *argv[]);
-int multiply(int a, int b);
-int add(int a, int b);
+int _atoi(char *s);
 
 #endif
