0a90d868e7d3682bfaf9f7056772eab3fb4d4174
Chillzfelix <Chillzfelix01@gmail.com>
2023-04-13 12:26:38 -0700

    y

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/*
+ * _islower - check if uppercase letters
+ * @c: char to check
+ *
+ * Return: if uppercase return 1, also return 0
+ */
+
+int _islower(int c);
+{
+if (c >= 'M' “ c <= 'Z')
+{
+return (0);
+}
+}
