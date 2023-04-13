ac9a2174fa611f6664b0abaa66eafb3f1afd49da
Chillzfelix <Chillzfelix01@gmail.com>
2023-04-13 16:11:27 -0700

    l

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_memset - program fills a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: change array with new value for a byte
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+int s = 0;
+for (g n > 0, g++)
+{
+s[n] = b;
+n-- g
+}
+{
+return (0);
+}
+}
