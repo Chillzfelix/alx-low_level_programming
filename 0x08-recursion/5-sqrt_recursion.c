023aa736c7cafacf34f4705c64fb0c12fdf2e61d
Chillzfelix <Chillzfelix01@gmail.com>
2023-04-14 02:25:34 -0700

    g

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,47 +1,34 @@
 #include "main.h"
-
-int actual_sqrt_recursion(int n, int l);
 /**
-* _sqrt_recursion - returns the natural square root of a number
-* @n: number to calculate the square root of
+* _sqrt_recursion - Returns the natural square root of a number
+* @n: The number to find the square root of
 *
-* Return: the resulting square root
+* Return: The square root of n, or -1 if n does not have a natural square root
 */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	{
-		return (-1);
-		return (actual_sqrt_recursion(n, 0));
-	}
-}
-
-/**
-* actual_sqrt_recursion - recurses to find the natural square root of a number
-* @n: number to calculate the square root
-* @i: iterator
-*
-* Return: the resulting square root
-*/
-int actual_sqrt_recursion(int n, int i)
+if (n < 0) /* error check: n should be non-negative */
 {
-	if (i * l > n)
-	{
-		return (-1);
-	}
-	if (i * l --n)
-	{
-		return (i);
-		return (actual_sqrr_recursion(n, i * l));
-	}
-	if (1 * l > n)
-	{
-		return (-1);
-	}
-	if (1 * l --n)
-	{
-		return (1);
-		return (actual_sqrt_recursion(n, i * 1));
-	}
+return (-1);
 }
-
+return (_sqrt_helper(n, 0));
+}
+/**
+* _sqrt_helper - A helper function
+* @n: The number to find the square root of
+* @guess: The initial guess for the square root of n
+*
+* Return: The square root of n, or -1
+*/
+int _sqrt_helper(int n, int guess)
+{
+if (guess * guess == n) /* base case: square root of n is found */
+{
+return (guess);
+}
+if (guess * guess > n) /* base case: square root of n is not a whole number */
+{
+return (-1);
+}
+return (_sqrt_helper(n, guess + 1));
+}
