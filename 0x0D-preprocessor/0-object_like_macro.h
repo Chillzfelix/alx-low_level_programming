dba08787aaf6b0ea1c91442f3bf8c6666c82633d
Chillzfelix <Chillzfelix01@gmail.com>
2023-04-17 21:02:56 -0700

    d

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,10 +1,6 @@
-#ifndef OBJECT_LIKE_MACRO.H
-#define {OBJECT_LIKE_MACRO.H}
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
-int SIZE(1024);
-{
-return (0);
-}
-#endif OBJECT_LIKE_MACRO.H
+#endif
--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,14 +1,6 @@
-#ifndef PI.H
-#define {PI.H}
+#ifndef PI_H
+#define PI_H
 
 #define PI 3.14159265359
-/**
- * PI - macro abbrevation named 3.14159265359
- *
- * Return: Always 0
- */
-int main(void)
-{
-return (0);
-}
+
 #endif
