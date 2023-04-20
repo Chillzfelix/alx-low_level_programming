710ca2bb73235654ce68b2122276f5428697c88f
Chillzfelix <Chillzfelix01@gmail.com>
2023-04-20 12:04:08 -0700

    h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,9 @@
-#ifndef VARIADIC_FUNCTIONS.H
-#define VARIADIC_FUNCTIONS.H
+#ifndef VARIADIC_H
+#define VARIADIC_H
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int _putchar(char c);
 
 #endif
