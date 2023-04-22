f6b16fb10eb3bb63fc4afe677e926a6a2d63702e
Chillzfelix <Chillzfelix01@gmail.com>
2023-04-22 05:57:59 -0700

    f

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,8 +9,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _putchar(char c);
-int _sqrt_helper(int n, int guess);
-int check_prime(int n, int i);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
