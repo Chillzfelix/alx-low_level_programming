84c3c188789a93fb9ae2fe49cfc0b3040bb1bd25
Chillzfelix <Chillzfelix01@gmail.com>
2023-04-27 11:41:33 -0700

    slingy

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -17,22 +17,19 @@ while (str[len])
 len++;
 new = malloc(sizeof(list_t));
 if (!new)
-{
-return (NULL);
 }
+return (NULL);
+{
 new->str = strdup(str);
 new->len = len;
 new->next = NULL;
 if (*head == NULL)
-{
+}
 *head = new;
-{
 return (new);
-}
-}
+{
 while (temp->next)
 temp = temp->next;
 temp->next = new;
-{
 return (new);
 }
