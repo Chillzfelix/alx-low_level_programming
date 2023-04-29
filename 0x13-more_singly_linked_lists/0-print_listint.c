a23c76697b93a27b7f323655d6e464c534c10fdc
Chillzfelix <Chillzfelix01@gmail.com>
2023-04-29 09:50:57 -0700

    slingy

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,8 @@
 include "lists.h"
+#include <stdio.h>
 /**
  * print_listint - prints all the elements of a linked list
- * @h: linked list to print
+ * @h: type listint_t of a linked list to print
  *
  * Return: number of nodes
  */
