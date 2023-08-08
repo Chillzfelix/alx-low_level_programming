e2b981d7f98988ada2a4d8b2d43b4acd0932cd8d
Chillzfelix <Chillzfelix01@gmail.com>
2023-08-07 17:00:43 -0700

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: File name
+ * @text_content: Content of file
+ * Return: 1 (Success)
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd, byte_written, len = 0;
+
+if (filename == NULL)
+return (-1);
+if (text_content != NULL)
+{
+for (len = 0; text_content[len];)
+len++;
+}
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+byte_written = write(fd, text_content, len);
+if (fd == -1 || byte_written == -1)
+return (-1);
+close(fd);
+return (1);
+}
